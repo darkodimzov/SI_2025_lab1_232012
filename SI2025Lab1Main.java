d071f3e779039038ee784e966a7fe4955e895332
Darko <darkodimzov@gmail.com>
2025-04-13 12:35:14 +0200

    find completed tasks implemented

--- SI2025Lab1Main.java
+++ SI2025Lab1Main.java
@@ -69,8 +69,13 @@ class TaskManager {
 
     // 2. Find all completed tasks
     public List<Task> getCompletedTasks() {
-        // TODO: Implement logic to return completed tasks
-        return new ArrayList<>();
+        List<Task> completed = new ArrayList<>();
+        for (Task t : tasks) {
+            if (t.isCompleted()) {
+                completed.add(t);
+            }
+        }
+        return completed;
     }
 
     // 3. List tasks sorted by name
@@ -120,6 +125,7 @@ public class SI2025Lab1Main {
         manager.addTask("Buy groceries", Priority.LOW, "Personal");
 
         // MISSING: Calls to the new methods that will be implemented
+        manager.getCompletedTasks();
 
         manager.printTasks();
     }
